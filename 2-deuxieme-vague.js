ca2209b142737640a035438f2db305a6715b5b96
Affholder <quentin@affholder.fr>
2025-09-16 01:11:33 +0200

    feat: finish ex2 (vague 2)

--- 2-deuxieme-vague.js
+++ 2-deuxieme-vague.js
@@ -14,7 +14,20 @@ console.log("Nom : " + personne.nom);
 console.log("Âge : " + personne.age);
 console.log("Ville : " + personne.ville);
 
+/*Exercice 2 :
+Créez un tableau personnes contenant les objets suivants :
+- {prenom: "John", nom: "Doe", age: 25, ville: "Rouen"}
+- {prenom: "Jane", nom: "Doe", age: 30, ville: "Paris"}
+- {prenom: "Jim", nom: "Doe", age: 35, ville: "Caen"}
+Affichez le tableau personnes dans la console avec log() et table().*/ 
 
+let personnes = [
+    {prenom: "John", nom: "Doe", age: 25, ville: "Rouen"},
+    {prenom: "Jane", nom: "Doe", age: 30, ville: "Paris"},
+    {prenom: "Jim", nom: "Doe", age: 35, ville: "Caen"}
+];
+console.log(personnes);
+console.table(personnes);
 
 
 
