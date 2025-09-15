dd1ed2767a4a2a08c826be9133021113900d73ea
Affholder <quentin@affholder.fr>
2025-09-16 01:02:56 +0200

    feat: finish ex1 (vague 2)

---
+++ 2-deuxieme-vague.js
@@ -0,0 +1,20 @@
+/*Exercice 1 :
+Créez un objet personne avec les propriétés vous concernant suivantes :
+prenom, nom, age, ville
+Affichez les propriétés prenom, nom, age et ville de l'objet personne dans
+des console.log() séparés.*/
+let personne = {
+    prenom: "Quentin",
+    nom: "Affholder",  
+    age: 18,
+    ville: "Lisieux"
+};
+console.log("Prénom : " + personne.prenom);
+console.log("Nom : " + personne.nom);
+console.log("Âge : " + personne.age);
+console.log("Ville : " + personne.ville);
+
+
+
+
+
