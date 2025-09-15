dca6c46db7280f913045e876454d4516f12c4973
Affholder <quentin@affholder.fr>
2025-09-15 20:04:57 +0200

    feat: finish ex1

---
+++ 1-premiere-vague.js
@@ -0,0 +1,9 @@
+/*Exercice 1 :
+Déclarez une variable pour stocker une température en degrés Celsius.
+Convertissez cette température en degrés Fahrenheit en utilisant la
+formule : F = C * 9/5 + 32.
+Affichez la température en degrés Celsius et Fahrenheit.*/
+
+let celcius = 25;
+let fahrenheit = celcius * 9/5 + 32;
+console.log(celcius + "°C est égal à " + fahrenheit + "°F");
