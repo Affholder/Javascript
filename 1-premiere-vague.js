86c292f7a1549dab9b171a49a7a6d3d505d18655
Affholder <quentin@affholder.fr>
2025-09-15 20:32:09 +0200

    feat: finish ex2

--- 1-premiere-vague.js
+++ 1-premiere-vague.js
@@ -7,3 +7,13 @@ Affichez la température en degrés Celsius et Fahrenheit.*/
 let celcius = 25;
 let fahrenheit = celcius * 9/5 + 32;
 console.log(celcius + "°C est égal à " + fahrenheit + "°F");
+
+/*Exercice 2 :
+Déclarez deux variables pour stocker la longueur et la largeur d'un
+rectangle.
+Calculez l'aire du rectangle en multipliant la longueur par la largeur.
+Affichez le résultat.*/ 
+let longueur = 10; 
+let largeur = 5;
+let aire = longueur * largeur;
+console.log("L'aire du rectangle est de " + aire + " unités carrées."); 
